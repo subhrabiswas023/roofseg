3dd87b0ae64d66515c26ac85f19bd4cd9cbe7f49
Subhra Biswas <subhrabiswas023@gmail.com>
2026-05-24 15:43:07 +0530

    [WIP] CI: bundle source code to a single file and push it to the kaggle

---
+++ scripts/generate_metadata.py
@@ -0,0 +1,31 @@
+import os
+import json
+from pathlib import Path
+
+KAGGLE_USERNAME = os.environ["KAGGLE_USERNAME"]
+KAGGLE_NOTEBOOK_SLUG = os.environ["KAGGLE_NOTEBOOK_SLUG"]
+KAGGLE_NOTEBOOK_TITLE = os.environ["KAGGLE_NOTEBOOK_TITLE"]
+
+DEFAULT_KERNEL_METADATA = {
+    "id": f"{KAGGLE_USERNAME}/{KAGGLE_NOTEBOOK_SLUG}", # FIX ME: This should be loaded from environment for transparancy across devices
+    "title": f"{KAGGLE_NOTEBOOK_TITLE}", # !!! Production enviornment secret
+    "code_file": "bundle.py",
+    "language": "python",
+    "kernel_type": "script",
+    "is_private": True,
+    "enable_gpu": True,
+    "enable_internet": True,
+    "dataset_sources": [
+        "dhruvpanchal1/inria-rooftop-segmentation-dataset-1024x1024-png"
+    ],
+}
+
+def serialize_metadata() -> None:
+    deployment_dir = Path("deploy")
+    deployment_dir.mkdir(parents=True, exist_ok=True)
+    
+    kernel_metadata_path = Path("deploy") / "kernel-metadata.json"
+    kernel_metadata_path.write_text(json.dumps(DEFAULT_KERNEL_METADATA))
+    
+if __name__ == '__main__':
+    serialize_metadata()
